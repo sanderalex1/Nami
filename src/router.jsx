5ceb58f9413639bc1936db08f914d19337a8ce57
sanderalex1 <oleksandr.kodehode@gmail.com>
2026-01-29 17:13:43 +0100

    update project configuration: set base path in vite.config.js, add deployment scripts in package.json, and adjust router path

--- src/router.jsx
+++ src/router.jsx
@@ -7,7 +7,7 @@ import ShoppingCart from "./pages/ShoppingCart";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/Nami/",
     element: <App />,
     children: [
       { index: true, element: <Home /> },
